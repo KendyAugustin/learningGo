db92072f17934617bf071d832e67724785e82e39
Kendy Augustin <86279167+KendyAugustin@users.noreply.github.com>
2022-04-02 12:35:01 -0700

    Create main.go
    Learning about variables

---
+++ 02_var/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//In order words. Variables are just place holders
+	// We can setup variables many ways in go
+
+	name := "Kendy" //This will store the value
+	age := 24
+
+	// also := this will assign the value type to the variable
+
+	fmt.Println("My name is ", name)
+	fmt.Println("My age is ", age)
+
+}
