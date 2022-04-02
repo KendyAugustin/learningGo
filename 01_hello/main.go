b94b070ace6d51815cff853b847a22afc76917d7
Kendy Augustin <86279167+KendyAugustin@users.noreply.github.com>
2022-04-02 11:59:36 -0700

    Update main.go

--- 01_hello/main.go
+++ 01_hello/main.go
@@ -1,7 +1,7 @@
-package main //We need to import the main package for 
+package main //We need to import the main package for every projetcs
 
-import "fmt"
+import "fmt" //This will allow us to import the fmt package in order to print stuffs out to the console
 
-func main(){
-	fmt.Println("Hello World")
+func main() { //The main func is need to run every programs
+	fmt.Println("Hello World") // will print out Hello World
 }
