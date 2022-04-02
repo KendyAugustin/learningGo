30fd4ac5ee799861e82fa8c6f2ecdd17822fc739
Kendy Augustin <86279167+KendyAugustin@users.noreply.github.com>
2022-04-02 13:12:55 -0700

    Update main.go

--- 02_var/main.go
+++ 02_var/main.go
@@ -7,6 +7,7 @@ func main() {
 	//In order words. Variables are just place holders
 	// We can setup variables many ways in go
 
+	var favoriteFood string = "Pizza"
 	name := "Kendy" //This will store the value
 	age := 24
 
@@ -14,5 +15,7 @@ func main() {
 
 	fmt.Println("My name is ", name)
 	fmt.Println("My age is ", age)
+
+	fmt.Printf("My name is %v, i'm %d years old and my favorite food is %v", name, age, favoriteFood)
 
 }
